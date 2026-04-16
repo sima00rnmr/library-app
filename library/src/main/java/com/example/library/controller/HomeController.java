9a24e4d5718358036bff7e09904f678be3db7ac7
sima00rnmr <sima92rnmr@gmail.com>
2026-04-16 19:38:10 +0900

    画面遷移作成途中

--- library/src/main/java/com/example/library/controller/HomeController.java
+++ library/src/main/java/com/example/library/controller/HomeController.java
@@ -10,4 +10,10 @@ public class HomeController {
     public String home() {
         return "forward:/home/index.html";
     }
+    @GetMapping("/rent")
+    public String rent() {
+        return "rent";
+    }
+ 
 }
+//ページは作成まだ
